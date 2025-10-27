9616b41108eea27b64c7216d0df9de87b20e2a8b
Richard Poole <richard.poole@cudoventures.com>
2025-10-27 11:20:24 +0000

    fix broken links

--- snippets/CliToolTable.jsx
+++ snippets/CliToolTable.jsx
@@ -1,69 +1,66 @@
 
 export const CliToolTable = () => {
-
-    const getOsImageIcon = (id) => {
-        const index = id.search('-')
-        const brand = index > 0 ? id.substring(0, index) : id
-        if (brand !== 'cudo') {
-            switch (brand) {
-                case 'macos':
-                    return '/images/osImages/macos.svg'
-                case 'debian':
-                    return '/images/osImages/debian.svg'
-                case 'redhat':
-                    return '/images/osImages/redhat.svg'
-                case 'linux':
-                    return '/images/osImages/linux.svg'
-                case 'windows':
-                    return '/images/osImages/windows.svg'
-            }
-        }
-        else {
-            switch (id) {
-                case 'cudo-ubuntu2004-docker':
-                    return '/images/osImages/docker.png'
-                case 'cudo-ubuntu2004-blender':
-                    return '/images/osImages/blender.png'
-                case 'cudo-tensorflow-docker-gpu':
-                    return '/images/osImages/tensorflow.svg'
-                case 'cudo-ubuntu-nvidia':
-                    return '/images/osImages/nvidia.svg'
-                case 'cudo-ubuntu-focal':
-                    return '/images/osImages/ubuntu.svg'
-                default:
-                    return '/logo-mark-black.svg'
-            }
-        }
+  const getOsImageIcon = (id) => {
+    const index = id.search('-')
+    const brand = index > 0 ? id.substring(0, index) : id
+    if (brand !== 'cudo') {
+      switch (brand) {
+        case 'macos':
+          return '/docs/images/os/macos.svg'
+        case 'debian':
+          return '/docs/images/os/debian.svg'
+        case 'redhat':
+          return '/docs/images/os/redhat.svg'
+        case 'linux':
+          return '/docs/images/os/linux.svg'
+        case 'windows':
+          return '/docs/images/os/windows.svg'
+      }
     }
+    else {
+      switch (id) {
+        case 'cudo-ubuntu2004-docker':
+          return '/docs/images/os/docker.png'
+        case 'cudo-ubuntu2004-blender':
+          return '/docs/images/os/blender.png'
+        case 'cudo-tensorflow-docker-gpu':
+          return '/docs/images/os/tensorflow.svg'
+        case 'cudo-ubuntu-nvidia':
+          return '/docs/images/os/nvidia.svg'
+        case 'cudo-ubuntu-focal':
+          return '/docs/images/os/ubuntu.svg'
+        default:
+          return '/logo-mark-black.svg'
+      }
+    }
+  }
 
+  const cliBinaries = [
+    { id: 'macos-amd64', name: 'macOS - amd64', platform: 'darwin', url: 'https://download.cudocompute.com/cli/cudoctl_darwin_amd64' },
+    { id: 'macos-arm64', name: 'macOS - arm64', platform: 'darwin', url: 'https://download.cudocompute.com/cli/cudoctl_darwin_arm64' },
+    { id: 'macos-universal', name: 'macOS - Universal', platform: 'darwin', url: 'https://download.cudocompute.com/cli/cudoctl_darwin_all' },
+    { id: 'debian-amd64', name: 'Debian - amd64', platform: 'debian', url: 'https://download.cudocompute.com/cli/cudoctl-amd64.deb' },
+    { id: 'debian-arm64', name: 'Debian - arm64', platform: 'debian', url: 'https://download.cudocompute.com/cli/cudoctl-arm64.deb' },
+    { id: 'redhat-amd64', name: 'RedHat (RPM) - amd64', platform: 'redhat', url: 'https://download.cudocompute.com/cli/cudoctl-amd64.rpm' },
+    { id: 'redhat-arm64', name: 'RedHat (RPM) - arm64', platform: 'redhat', url: 'https://download.cudocompute.com/cli/cudoctl-arm64.rpm' },
+    { id: 'linux-amd64', name: 'Other Linux - amd64', platform: 'linux', url: 'https://download.cudocompute.com/cli/cudoctl_linux_amd64.tar.gz' },
+    { id: 'linux-arm64', name: 'Other Linux - arm64', platform: 'linux', url: 'https://download.cudocompute.com/cli/cudoctl_linux_arm64.tar.gz' },
+    { id: 'windows-amd64', name: 'Windows', platform: 'windows', url: 'https://download.cudocompute.com/cli/cudoctl_windows_amd64.zip' },
+  ]
 
+  return (
+    <Columns cols={3}>
+      {cliBinaries.map(bin => (
+        <Card
+          key={bin.id}
+          href={bin.url}
 
-    const cliBinaries = [
-        { id: 'macos-amd64', name: 'macOS - amd64', platform: 'darwin', url: 'https://download.cudocompute.com/cli/cudoctl_darwin_amd64' },
-        { id: 'macos-arm64', name: 'macOS - arm64', platform: 'darwin', url: 'https://download.cudocompute.com/cli/cudoctl_darwin_arm64' },
-        { id: 'macos-universal', name: 'macOS - Universal', platform: 'darwin', url: 'https://download.cudocompute.com/cli/cudoctl_darwin_all' },
-        { id: 'debian-amd64', name: 'Debian - amd64', platform: 'debian', url: 'https://download.cudocompute.com/cli/cudoctl-amd64.deb' },
-        { id: 'debian-arm64', name: 'Debian - arm64', platform: 'debian', url: 'https://download.cudocompute.com/cli/cudoctl-arm64.deb' },
-        { id: 'redhat-amd64', name: 'RedHat (RPM) - amd64', platform: 'redhat', url: 'https://download.cudocompute.com/cli/cudoctl-amd64.rpm' },
-        { id: 'redhat-arm64', name: 'RedHat (RPM) - arm64', platform: 'redhat', url: 'https://download.cudocompute.com/cli/cudoctl-arm64.rpm' },
-        { id: 'linux-amd64', name: 'Other Linux - amd64', platform: 'linux', url: 'https://download.cudocompute.com/cli/cudoctl_linux_amd64.tar.gz' },
-        { id: 'linux-arm64', name: 'Other Linux - arm64', platform: 'linux', url: 'https://download.cudocompute.com/cli/cudoctl_linux_arm64.tar.gz' },
-        { id: 'windows-amd64', name: 'Windows', platform: 'windows', url: 'https://download.cudocompute.com/cli/cudoctl_windows_amd64.zip' },
-    ]
-
-    return (
-        <Columns cols={3}>
-            {cliBinaries.map(bin => (
-                <Card
-                    key={bin.id}
-                    href={bin.url}
-
-                >
-                    <Icon icon={getOsImageIcon(bin.id)} size={18} color="transparent" alt={bin.name} />
-                    <span style={{ marginLeft: '.4rem', fontSize: '0.8rem' }}>{bin.name}</span>
-                </Card>
-            ))}
-        </Columns>
-    );
+        >
+          <Icon icon={getOsImageIcon(bin.id)} size={18} color="transparent" alt={bin.name} />
+          <span style={{ marginLeft: '.4rem', fontSize: '0.8rem' }}>{bin.name}</span>
+        </Card>
+      ))}
+    </Columns>
+  )
 }
 
--- snippets/PublicImagesGrid.jsx
+++ snippets/PublicImagesGrid.jsx
@@ -1,110 +1,107 @@
-
 export const PublicImagesGrid = () => {
+  const getOsImageIcon = (id) => {
+    if (/stability/.test(id))
+      return '/docs/images/os/stability-ai.svg'
 
-    const getOsImageIcon = (id) => {
-        if (/stability/.test(id))
-            return '/images/osImages/stability-ai.svg'
-
-        const index = id.search('-')
-        const brand = index > 0 ? id.substring(0, index) : id
-        if (brand !== 'cudo') {
-            switch (brand) {
-                case 'alpine':
-                    return '/images/osImages/alpine-linux.png'
-                case 'amazon':
-                    return '/images/osImages/amazon-linux.png'
-                case 'centos':
-                    return '/images/osImages/centos.svg'
-                case 'debian':
-                    return '/images/osImages/debian.svg'
-                case 'devuan':
-                    return '/images/osImages/devuan.png'
-                case 'fedora':
-                    return '/images/osImages/fedora.png'
-                // todo: this id should be hyphonated in db
-                case 'fedora36':
-                    return '/images/osImages/fedora.png'
-                case 'freebsd':
-                    return '/images/osImages/freebsd.svg'
-                case 'gitlab':
-                    return '/images/osImages/gitlab.svg'
-                case 'hive':
-                    return '/images/osImages/hive.png'
-                case 'opensuse':
-                    return '/images/osImages/open-suse.png'
-                case 'oracle':
-                    return '/images/osImages/oracle.svg'
-                case 'rocky':
-                    return '/images/osImages/rocky-linux.png'
-                case 'service':
-                    return '/images/osImages/wordpress.png'
-                case 'ubuntu':
-                    return '/images/osImages/ubuntu.svg'
-                case 'windows':
-                    return '/images/osImages/windows.svg'
-            }
-        }
-        else {
-            switch (id) {
-                case 'cudo-ubuntu2004-docker':
-                    return '/images/osImages/docker.png'
-                case 'cudo-ubuntu2004-blender':
-                    return '/images/osImages/blender.png'
-                case 'cudo-tensorflow-docker-gpu':
-                    return '/images/osImages/tensorflow.svg'
-                case 'cudo-ubuntu-nvidia':
-                    return '/images/osImages/nvidia.svg'
-                case 'cudo-ubuntu-focal':
-                    return '/images/osImages/ubuntu.svg'
-                default:
-                    return '/logo-mark-black.svg'
-            }
-        }
+    const index = id.search('-')
+    const brand = index > 0 ? id.substring(0, index) : id
+    if (brand !== 'cudo') {
+      switch (brand) {
+        case 'alpine':
+          return '/docs/images/os/alpine-linux.png'
+        case 'amazon':
+          return '/docs/images/os/amazon-linux.png'
+        case 'centos':
+          return '/docs/images/os/centos.svg'
+        case 'debian':
+          return '/docs/images/os/debian.svg'
+        case 'devuan':
+          return '/docs/images/os/devuan.png'
+        case 'fedora':
+          return '/docs/images/os/fedora.png'
+        // todo: this id should be hyphonated in db
+        case 'fedora36':
+          return '/docs/images/os/fedora.png'
+        case 'freebsd':
+          return '/docs/images/os/freebsd.svg'
+        case 'gitlab':
+          return '/docs/images/os/gitlab.svg'
+        case 'hive':
+          return '/docs/images/os/hive.png'
+        case 'opensuse':
+          return '/docs/images/os/open-suse.png'
+        case 'oracle':
+          return '/docs/images/os/oracle.svg'
+        case 'rocky':
+          return '/docs/images/os/rocky-linux.png'
+        case 'service':
+          return '/docs/images/os/wordpress.png'
+        case 'ubuntu':
+          return '/docs/images/os/ubuntu.svg'
+        case 'windows':
+          return '/docs/images/os/windows.svg'
+      }
     }
+    else {
+      switch (id) {
+        case 'cudo-ubuntu2004-docker':
+          return '/docs/images/os/docker.png'
+        case 'cudo-ubuntu2004-blender':
+          return '/docs/images/os/blender.png'
+        case 'cudo-tensorflow-docker-gpu':
+          return '/docs/images/os/tensorflow.svg'
+        case 'cudo-ubuntu-nvidia':
+          return '/docs/images/os/nvidia.svg'
+        case 'cudo-ubuntu-focal':
+          return '/docs/images/os/ubuntu.svg'
+        default:
+          return '/logo-mark-black.svg'
+      }
+    }
+  }
 
+  const publicImages = [
+    { id: 'alpine-linux-319', name: 'Alpine Linux 3.19', description: 'Security-oriented, lightweight Linux distribution based on musl libc and busybox', displayName: '', platform: 'linux', sizeGib: 1, installedPackages: [] },
+    { id: 'centos-7', name: 'CentOS 7', description: 'CentOS 7', displayName: '', platform: 'linux', sizeGib: 8, installedPackages: [] },
+    { id: 'centos-8-stream', name: 'CentOS 8 Stream', description: 'CentOS 8 Stream', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
+    { id: 'debian-11', name: 'Debian 11', description: 'Debian GNU/Linux 11 (bullseye)', displayName: '', platform: 'linux', sizeGib: 2, installedPackages: [] },
+    { id: 'debian-12', name: 'Debian 12', description: 'Debian GNU/Linux 12 (bookworm)', displayName: '', platform: 'linux', sizeGib: 2, installedPackages: [] },
+    { id: 'fedora-37', name: 'Fedora 37', description: 'Fedora 37', displayName: '', platform: 'linux', sizeGib: 20, installedPackages: [] },
+    { id: 'freebsd-13', name: 'FreeBSD 13', description: 'FreeBSD 13', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
+    { id: 'opensuse-15', name: 'openSUSE 15', description: 'openSUSE 15', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
+    { id: 'rocky-linux-9', name: 'Rocky Linux 9', description: 'Rocky Linux 9.1', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
+    { id: 'ubuntu-2004', name: 'Ubuntu 20.04', description: 'Ubuntu 20.04 LTS (focal)', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
+    { id: 'ubuntu-2204', name: 'Ubuntu 22.04', description: 'Ubuntu 22.04 LTS (jammy)', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
+    { id: 'ubuntu-2204-nvidia-535-docker-v20241017', name: 'Ubuntu 22.04 + NVIDIA v535 drivers + Docker', description: 'Ubuntu 22.04 with NVIDIA drivers and Docker preinstalled', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
+    { id: 'ubuntu-2204-nvidia-550-docker-v20250303', name: 'Ubuntu 22.04 + NVIDIA v550 drivers + Docker', description: 'Ubuntu 22.04 with NVIDIA drivers and Docker preinstalled', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
+    { id: 'ubuntu-2404', name: 'Ubuntu 24.04', description: 'Ubuntu 24.04 LTS (noble)', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
+    { id: 'windows-10-pro', name: 'Windows 10 Pro', description: 'Windows 10 Pro', displayName: '', platform: 'windows', sizeGib: 50, installedPackages: [] },
+    { id: 'windows-11-pro', name: 'Windows 11 Pro', description: 'Windows 11 Pro', displayName: '', platform: 'windows', sizeGib: 50, installedPackages: [] },
+  ]
 
-    const publicImages = [
-        { id: 'alpine-linux-319', name: 'Alpine Linux 3.19', description: 'Security-oriented, lightweight Linux distribution based on musl libc and busybox', displayName: '', platform: 'linux', sizeGib: 1, installedPackages: [] },
-        { id: 'centos-7', name: 'CentOS 7', description: 'CentOS 7', displayName: '', platform: 'linux', sizeGib: 8, installedPackages: [] },
-        { id: 'centos-8-stream', name: 'CentOS 8 Stream', description: 'CentOS 8 Stream', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
-        { id: 'debian-11', name: 'Debian 11', description: 'Debian GNU/Linux 11 (bullseye)', displayName: '', platform: 'linux', sizeGib: 2, installedPackages: [] },
-        { id: 'debian-12', name: 'Debian 12', description: 'Debian GNU/Linux 12 (bookworm)', displayName: '', platform: 'linux', sizeGib: 2, installedPackages: [] },
-        { id: 'fedora-37', name: 'Fedora 37', description: 'Fedora 37', displayName: '', platform: 'linux', sizeGib: 20, installedPackages: [] },
-        { id: 'freebsd-13', name: 'FreeBSD 13', description: 'FreeBSD 13', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
-        { id: 'opensuse-15', name: 'openSUSE 15', description: 'openSUSE 15', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
-        { id: 'rocky-linux-9', name: 'Rocky Linux 9', description: 'Rocky Linux 9.1', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
-        { id: 'ubuntu-2004', name: 'Ubuntu 20.04', description: 'Ubuntu 20.04 LTS (focal)', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
-        { id: 'ubuntu-2204', name: 'Ubuntu 22.04', description: 'Ubuntu 22.04 LTS (jammy)', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
-        { id: 'ubuntu-2204-nvidia-535-docker-v20241017', name: 'Ubuntu 22.04 + NVIDIA v535 drivers + Docker', description: 'Ubuntu 22.04 with NVIDIA drivers and Docker preinstalled', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
-        { id: 'ubuntu-2204-nvidia-550-docker-v20250303', name: 'Ubuntu 22.04 + NVIDIA v550 drivers + Docker', description: 'Ubuntu 22.04 with NVIDIA drivers and Docker preinstalled', displayName: '', platform: 'linux', sizeGib: 10, installedPackages: [] },
-        { id: 'ubuntu-2404', name: 'Ubuntu 24.04', description: 'Ubuntu 24.04 LTS (noble)', displayName: '', platform: 'linux', sizeGib: 4, installedPackages: [] },
-        { id: 'windows-10-pro', name: 'Windows 10 Pro', description: 'Windows 10 Pro', displayName: '', platform: 'windows', sizeGib: 50, installedPackages: [] },
-        { id: 'windows-11-pro', name: 'Windows 11 Pro', description: 'Windows 11 Pro', displayName: '', platform: 'windows', sizeGib: 50, installedPackages: [] },
-    ]
-
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th style={{ textAlign: "left" }}>Name</th>
-                    <th style={{ textAlign: "right" }}>Size</th>
-                </tr>
-            </thead>
-            <tbody>
-                {publicImages.map(img => (
-                    <tr key={img.id}>
-                        <td>
-                            <span style={{ display: 'flex', alignItems: 'center', gap: '0.25rem' }}>
-                                <Icon icon={getOsImageIcon(img.id)} color="transparent" size={18} alt={img.name} />
-                                <span>{img.name}</span>
-                                <code style={{ background: 'none', fontSize: '0.75rem' }}>{img.id}</code>
-                            </span>
-                        </td>
-                        <td style={{ textAlign: 'right' }}>{img.sizeGib} GB</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table >
-    );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th style={{ textAlign: "left" }}>Name</th>
+          <th style={{ textAlign: "right" }}>Size</th>
+        </tr>
+      </thead>
+      <tbody>
+        {publicImages.map(img => (
+          <tr key={img.id}>
+            <td>
+              <span style={{ display: 'flex', alignItems: 'center', gap: '0.25rem' }}>
+                <Icon icon={getOsImageIcon(img.id)} color="transparent" size={18} alt={img.name} />
+                <span>{img.name}</span>
+                <code style={{ background: 'none', fontSize: '0.75rem' }}>{img.id}</code>
+              </span>
+            </td>
+            <td style={{ textAlign: 'right' }}>{img.sizeGib} GB</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
